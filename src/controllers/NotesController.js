b192bb16d3c14c4c3084fcaabed7f1b3ec6a68e7
vinizbjj <viniz.bjj@gmail.com>
2022-10-31 04:53:14 -0300

    finalizando projeto,

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -26,10 +26,74 @@ class NotesController {
                 user_id
             }
         });
-        
+
         await knex("tags").insert(tagsInsert);
 
         response.json();
+    }
+
+    async show(request, response) {
+        const { id } = request.params;
+
+        const note = await knex("notes").where({ id }).first();
+        const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+        const links = await knex("links").where({ note_id: id }).orderBy("created_at");
+
+        return response.json({
+            ...note,
+            tags,
+            links
+        });
+    }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("notes").where({ id }).delete();
+
+        return response.json();
+    }
+
+    async index(request, response) {
+        const { title, user_id, tags } = request.query;
+
+        let notes;
+
+        if (tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim());
+
+            notes = await knex("tags")
+                .select([
+                    "notes.id",
+                    "notes.title",
+                    "notes.user_id",
+                ])
+                .where("notes.user_id", user_id)
+                .whereLike("notes.title", `%${title}%`)
+                .whereIn("name", filterTags)
+                .innerJoin("notes", "notes.id", "tags.note_id")
+                .orderBy("notes.title")
+
+
+        } else {
+            notes = await knex("notes")
+                .where({ user_id })
+                .whereLike("title", `%${title}%`)
+                .orderBy("title");
+        }
+
+        const userTags = await knex("tags").where({ user_id });
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        })
+
+
+        return response.json(notesWithTags)
     }
 }
 
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -6,7 +6,10 @@ const notesRoutes = Router();
 
 const notesController = new NotesController();
 
+notesRoutes.get("/", notesController.index);
+notesRoutes.get("/:id", notesController.show);
 notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.delete("/:id", notesController.delete);
 
 
 module.exports = notesRoutes;
